d4ee80580a7f8935fed8cbbcd7073f1f7bdadf96
Devrajj-14 <dg9770@srmist.edu.in>
2026-01-13 17:13:17 +0530

    final d2 commit

--- Java/java_program_elemnt_practice/program.java
+++ Java/java_program_elemnt_practice/program.java
@@ -9,16 +9,3 @@ package Java.java_program_elemnt_practice;
         }
     }
 
-//Question 2
-
-    class PCM {
-        public static void main(String[] args) {
-            String name = "Sam";
-            int maths = 94;
-            int physics = 95;
-            int chemistry = 96;
-
-            int average = (maths + physics + chemistry) / 3;
-            System.out.println(name + " average mark in PCM is " + average);
-        }
-    }
